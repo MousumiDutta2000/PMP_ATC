12771031bb94b05e6a880147bfc5864705e81567
Mousumi Dutta <97080528+MousumiDutta2000@users.noreply.github.com>
2023-06-15 14:28:44 +0530

    Project Management Project

--- PMP_ATC/crud/app/Models/ProjectMember.php
+++ PMP_ATC/crud/app/Models/ProjectMember.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProjectMember extends Model
 {
+    use HasFactory;
+
+    protected $table = 'project_member';
+
     protected $fillable = [
         'is_active',
         'user_id',
--- PMP_ATC/crud/app/Models/ProjectRole.php
+++ PMP_ATC/crud/app/Models/ProjectRole.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProjectRole extends Model
 {
+    use HasFactory;
+
+    protected $table = 'project_role';
+
     protected $fillable = ['member_role_type'];
 }
