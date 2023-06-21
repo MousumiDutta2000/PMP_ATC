b9c91c9a40e9995aacf3db2be46f578fe007e6aa
Mousumi Dutta <97080528+MousumiDutta2000@users.noreply.github.com>
2023-06-21 13:01:44 +0530

    Project create page updated

--- PMP_ATC/crud/resources/views/projects/create.blade.php
+++ PMP_ATC/crud/resources/views/projects/create.blade.php
@@ -1,110 +1,80 @@
-@extends('layouts.side_nav') @section('content')
-<h1>Create Project</h1>
+@extends('layouts.side_nav') @section('pageTitle', 'Projects')
+@section('content')
 
-@if ($errors->any())
-<div>
-    <strong>Validation Errors:</strong>
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">{{ $project->project_name }}/Settings</li>
+    </ol>
+</nav>
 
-<form action="{{ route('projects.store') }}" method="POST">
-    @csrf
-    <div class="form-group">
-        <label for="project_name">Project Name:</label>
+<form
+    action="{{ route('projects.updateSettings', $project->id) }}"
+    method="POST"
+    class="form">
+
+    @csrf @method('PUT')
+
+    <div class="mb-3">
+        <label for="projectIdInput" class="form-label">Project ID</label>
         <input
             type="text"
+            id="projectIdInput"
+            class="form-control"
+            name="project_id"
+            value="{{ $project->id }}"
+            required="required">
+    </div>
+
+    <div class="mb-3">
+        <label for="projectNameInput" class="form-label">Project Name</label>
+        <input
+            type="text"
+            id="projectNameInput"
+            class="form-control"
             name="project_name"
-            id="project_name"
-            class="form-control"
+            value="{{ $project->project_name }}"
             required="required">
     </div>
-    <div class="form-group">
-        <label for="typeSelect" class="form-label">Project Type:</label>
-        <select
-            id="typeSelect"
-            class="form-select"
-            name="project_type"
-            required="required">
-            <option value="" selected="selected" disabled="disabled">Type</option>
-            <option>Internal</option>
-            <option>External</option>
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="project_description">Description:</label>
+
+    <div class="mb-3">
+        <label for="projectDescriptionInput" class="form-label">Project Description</label>
         <input
             type="text"
+            id="projectDescriptionInput"
+            class="form-control"
             name="project_description"
-            id="project_description"
-            class="form-control"
+            value="{{ $project->project_description }}"
+            placeholder="Describe the project"
             required="required">
     </div>
-    <div class="form-group">
-        <label for="project_manager">Project Manager:</label>
-        <input
-            type="text"
-            name="project_manager"
-            id="project_manager"
-            class="form-control"
-            required="required">
+
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <label for="projectStartInput" class="form-label">Project Start</label>
+            <input
+                type="date"
+                id="projectStartInput"
+                class="form-control"
+                name="project_start"
+                value="{{ $project->project_start }}"
+                required="required">
+        </div>
+        <div class="col-md-6 mb-3">
+            <label for="projectEndInput" class="form-label">Project End</label>
+            <input
+                type="date"
+                id="projectEndInput"
+                class="form-control"
+                name="project_end"
+                value="{{ $project->project_end }}"
+                required="required">
+        </div>
     </div>
-    <div class="form-group">
-        <label for="project_startDate">Project StartDate:</label>
-        <input
-            type="date"
-            name="project_startDate"
-            id="project_startDate"
-            class="form-control"
-            required="required">
-    </div>
-    <div class="form-group">
-        <label for="project_endDate">Project EndDate:</label>
-        <input
-            type="date"
-            name="project_endDate"
-            id="project_endDate"
-            class="form-control"
-            required="required">
-    </div>
-    <div class="form-group">
-        <label for="client_spoc_name">Client Name:</label>
-        <input
-            type="text"
-            name="client_spoc_name"
-            id="client_spoc_name"
-            class="form-control"
-            required="required">
-    </div>
-    <div class="form-group">
-        <label for="client_spoc_email">Client Email:</label>
-        <input
-            type="email"
-            name="client_spoc_email"
-            id="client_spoc_email"
-            class="form-control"
-            required="required">
-    </div>
-    <div class="form-group">
-        <label for="client_spoc_contact">Client Contact:</label>
-        <input
-            type="text"
-            name="client_spoc_contact"
-            id="client_spoc_contact"
-            class="form-control"
-            required="required">
-    </div>
-    <div class="form-group">
+
+    <div class="mb-3">
         <label for="statusSelect" class="form-label">Status</label>
-        <select
-            id="statusSelect"
-            class="form-select"
-            name="project_status"
-            required="required">
+        <select id="statusSelect" class="form-select" name="status">
+
             <option value="" selected="selected" disabled="disabled">Status</option>
             <option>Not Started</option>
             <option>Delay</option>
@@ -114,34 +84,22 @@
         </select>
     </div>
 
-    <div class="form-group">
-        <label for="">Vertical id:</label>
-        <input
-            type="text"
-            name="vertical_id"
-            id="vertical_id"
-            class="form-control"
-            required="required">
-    </div>
-    <div class="form-group">
-        <label for="">Technologies id:</label>
-        <input
-            type="text"
-            name="technologies_id"
-            id="technologies_id"
-            class="form-control"
-            required="required">
-    </div>
-    <div class="form-group">
-        <label for="">Client id:</label>
-        <input
-            type="text"
-            name="clients_id"
-            id="client_id"
-            class="form-control"
-            required="required">
+    <div class="mb-3">
+        <label for="memberInput" class="form-label">Member</label>
+        <i class="fas fa-plus plus-icon" id="plusSign"></i>
     </div>
 
-    <button type="submit" class="btn btn-primary">Create</button>
+    <div id="popupContainer" style="display: none;">
+        <div id="popupContent">
+            <input type="text" id="searchInput" class="form-control" placeholder="Search">
+            <div id="searchResults"></div>
+        </div>
+        <div id="popupOverlay"></div>
+    </div>
+
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Save</button>
+        <a href="{{ route('projects.index') }}" class="btn btn-secondary">Cancel</a>
+    </div>
 </form>
 @endsection
