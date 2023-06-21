4645a9e4868ddbd93bb80bd5681ca44555165ec7
masc4424 <arrkya22@gmail.com>
2023-06-21 13:13:33 +0530

    profile table update

--- PMP_ATC/crud/resources/views/profiles/index.blade.php
+++ PMP_ATC/crud/resources/views/profiles/index.blade.php
@@ -8,17 +8,14 @@
 @endsection
 
 @section('custom_css')
-
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
     <link rel='stylesheet' href='https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css'>
     <link rel='stylesheet' href='https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css'>
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/boxicons@2.0.0/css/boxicons.min.css'>
     <link rel="stylesheet" href="{{ asset('css/table.css') }}">
-
 @endsection
 
 @section('custom_js')
-
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
@@ -26,7 +23,6 @@
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
     <script src="{{ asset('js/table.js') }}"></script>
-
 @endsection
 
 @section('content')
@@ -41,82 +37,58 @@
                     <tr>
                         <th>Client Name</th>
                         <th>Phone Number</th>
-                        <th>Profession</th>
+                        <th>Line Manager</th>
+                        <th>User</th>
+                        <th>Vertical</th>
+                        <th>Designation</th>
                         <th>Date of Birth</th>
-                        <th>App Access</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                        <a href="#">
-                            <div class="d-flex align-items-center">
-                            <div class="avatar avatar-blue mr-3">{{$profile->image}}</div>
-
-                            <div class="">
-                                <p class="font-weight-bold mb-0">{{$profile->name}}</p>
-                                <p class="text-muted mb-0">{{$profile->email}}</p>
-                            </div>
-                            </div>
-                        </a>
-                        </td>
-                        <td>{{$profile->contact_number}}</td>
-                        <td>{{$profile->designation_id}}</td>
-                        <td>09/04/1996</td>
-                        <td>
-                        <div class="badge badge-success badge-success-alt">Enabled</div>
-                        </td>
-                        <td>
-                        <div class="dropdown">
-                            <button class="btn btn-sm btn-icon" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="bx bx-dots-horizontal-rounded" data-toggle="tooltip" data-placement="top"
-                                    title="Actions"></i>
-                                </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                            <a class="dropdown-item" href="#"><i class="bx bxs-pencil mr-2"></i> Edit Profile</a>
-                            <a class="dropdown-item text-danger" href="#"><i class="bx bxs-trash mr-2"></i> Remove</a>
-                            </div>
-                        </div>
-                        </td>
-                    </tr>
-
-                    <tr>
-                     <td>
-                        <a href="#">
-                            <div class="d-flex align-items-center">
-                            <div class="avatar avatar-pink mr-3">JR</div>
-
-                            <div class="">
-                                <p class="font-weight-bold mb-0">Julie Richards</p>
-                                <p class="text-muted mb-0">julie_89@example.com</p>
-                            </div>
-                            </div>
-                        </a>
-                        </td>
-                        <td> (937) 874 6878</td>
-                        <td>Investment Banker</td>
-                        <td>13/01/1989</td>
-                        <td>
-                        <div class="badge badge-success badge-success-alt">Enabled</div>
-                        </td>
-                        <td>
-                        <div class="dropdown">
-                            <button class="btn btn-sm btn-icon" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="bx bx-dots-horizontal-rounded" data-toggle="tooltip" data-placement="top"
-                                    title="Actions"></i>
-                                </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                            <a class="dropdown-item" href="#"><i class="bx bxs-pencil mr-2"></i> Edit Profile</a>
-                            <a class="dropdown-item text-danger" href="#"><i class="bx bxs-trash mr-2"></i> Remove</a>
-                            </div>
-                        </div>
-                        </td>
-                    </tr>
+                    @if(count($profiles)>0)
+                        @foreach($profiles as $profile)
+                            <tr>
+                                <td>
+                                    <a href="#">
+                                        <div class="d-flex align-items-center">
+                                            <div class="avatar avatar-blue mr-3">{{$profile->image}}</div>
+                                            <div class="">
+                                                <p class="font-weight-bold mb-0">{{$profile->name}}</p>
+                                                <p class="text-muted mb-0">{{$profile->email}}</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </td>
+                                <td>{{$profile->contact_number}}</td>
+                                <td>{{$profile->line_manager_id}}</td>
+                                <td>{{$profile->user_id}}</td>
+                                <td>{{$profile->vertical_id}}</td>
+                                <td>{{$profile->designation_id}}</td>
+                                <td>09/04/1996</td>
+                                <td>
+                                    <div class="btn-group" role="group">
+                                        <a href="{{ route('profiles.show', ['profile' => $profile->id]) }}">
+                                            <i class="fas fa-eye text-info" style="margin-right: 10px"></i>
+                                        </a>
+                                        <a href="{{ route('profiles.edit', ['profile' => $profile->id]) }}">
+                                            <i class="fas fa-edit text-primary" style="margin-right: 10px"></i>
+                                        </a>
+                                        <form method="post" action="{{ route('profiles.destroy', ['profile' => $profile->id]) }}">
+                                            @method('delete')
+                                            @csrf
+                                            <button type="submit" class="btn btn-link p-0">
+                                                <i class="fas fa-trash-alt text-danger" style="border: none;"></i>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endif
                 </tbody>
             </table>
         </div>
     </section>
 </main>
 @endsection
-
