32a6ae06f74bae194d328cf94b3cac0f2428ee5c
Mousumi Dutta <97080528+MousumiDutta2000@users.noreply.github.com>
2023-06-21 08:59:25 +0530

    Modify the project index page

--- PMP_ATC/crud/resources/views/projects/index.blade.php
+++ PMP_ATC/crud/resources/views/projects/index.blade.php
@@ -10,34 +10,77 @@
         </ol>
     </nav>
 
+    <div class="row">
+        <div class="col-md-6">
+            <form action="{{ route('projects.index') }}" method="GET">
+                <div class="input-group mb-3">
+                    <select class="form-control" name="search_criteria">
+                        <option value="date">Search by date</option>
+                        <option value="technology">Search by technology</option>
+                    </select>
+                    <input type="text" class="form-control" name="search_value" placeholder="Enter search value">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-primary" type="submit">Search</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
     <a href="{{ route('projects.create') }}" class="btn btn-primary">Create project</a>
 
     <table class="table">
         <thead>
             <tr>
-                <th>Project ID </th>
+                <th>Project ID</th>
                 <th>Project Name</th>
+                <th>Status</th>
                 <th>Actions</th>
-                <!-- <th>Project Status</th> -->
             </tr>
         </thead>
         <tbody>
             @foreach ($projects as $project)
-                <tr>
-                    <td>{{ $project->id }}</td>
-                    <td>{{ $project->project_name }}</td>
-                    <td>
-                        <a href="" class="btn btn-info">Manage</a>
-                        <a href="" class="btn btn-primary">Report</a>
-                        <a href="{{ route('projects.settings', $project->id) }}" class="btn btn-primary">Settings</a>
+                @if (isset($_GET['search_criteria']) && isset($_GET['search_value']))
+                    @php
+                        $searchCriteria = $_GET['search_criteria'];
+                        $searchValue = $_GET['search_value'];
+                    @endphp
+                    @if (($searchCriteria === 'date' && $project->date == $searchValue) || ($searchCriteria === 'technology' && $project->technology == $searchValue))
+                        <tr>
+                            <td>{{ $project->id }}</td>
+                            <td>{{ $project->project_name }}</td>
+                            <td>{{ $project->active ? 'Active' : 'Inactive' }}</td>
+                            <td>
+                                <a href="" class="btn btn-info">Manage</a>
+                                <a href="" class="btn btn-primary">Report</a>
+                                <a href="{{ route('projects.settings', $project->id) }}" class="btn btn-primary">Settings</a>
 
-                        <form action="{{ route('projects.destroy', $project->id) }}" method="POST" class="d-inline">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
+                                <form action="{{ route('projects.destroy', $project->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endif
+                @else
+                    <tr>
+                        <td>{{ $project->id }}</td>
+                        <td>{{ $project->project_name }}</td>
+                        <td>{{ $project->active ? 'Active' : 'Inactive' }}</td>
+                        <td>
+                            <a href="" class="btn btn-info">Manage</a>
+                            <a href="" class="btn btn-primary">Report</a>
+                            <a href="{{ route('projects.settings', $project->id) }}" class="btn btn-primary">Settings</a>
+
+                            <form action="{{ route('projects.destroy', $project->id) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endif
             @endforeach
         </tbody>
     </table>
