3dc960008e346360d093ce9f75fa22fbc9cff9e1
shampoo3 <sampurnaghosh3008@gmail.com>
2023-06-23 00:08:35 +0530

    user-technologies table added

---
+++ PMP_ATC/crud/database/migrations/2023_06_22_174128_create_user_technologies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_technologies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('project_role_id');
+            $table->foreign('project_role_id')->references('id')->on('project_role');
+            $table->unsignedBigInteger('technology_id');
+            $table->foreign('technology_id')->references('id')->on('technologies');
+            $table->string('details');
+            $table->string('years_of_experience');
+            $table->boolean('is_current_company');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_technologies');
+    }
+};
