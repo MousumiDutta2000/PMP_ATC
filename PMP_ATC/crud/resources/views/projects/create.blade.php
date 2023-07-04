e164cf0be4fc354c9b1e250b4b25fc1458a3541d
masc4424 <arrkya22@gmail.com>
2023-07-04 09:00:20 +0530

    #

--- PMP_ATC/crud/resources/views/projects/create.blade.php
+++ PMP_ATC/crud/resources/views/projects/create.blade.php
@@ -189,7 +189,7 @@
                                     <select id="project_members_id" name="project_members_id[]" class="js-example-basic-single" required style="width:100%;">
                                         <option value="">Select Member</option>
                                         @foreach($projectMembers as $projectMember)
-                                        <option value="{{ $projectMember->id }}">{{ $projectMember->profileName->name }}</option>
+                                        <option value="{{ $projectMember->id }}">{{ $projectMember->profile_name }}</option>
                                         @endforeach
                                     </select>
                                 </div>
@@ -234,7 +234,7 @@
                                 <select id="edit_project_members_id" name="project_members_id" class="select" required style="width:100%;">
                                     <option value="">Select Member</option>
                                     @foreach($projectMembers as $projectMember)
-                                    <option value="{{ $projectMember->id }}">{{ $projectMember->profileName->name }}</option>
+                                    <option value="{{ $projectMember->id }}">{{ $projectMember->profile_name }}</option>
                                     @endforeach
                                 </select>
                             </div>
