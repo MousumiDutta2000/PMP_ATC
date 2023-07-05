95e813a2b751f61848f0c81efa30f60b745468f5
shampoo3 <sampurnaghosh3008@gmail.com>
2023-07-05 11:39:53 +0530

    skill edit modal added

--- PMP_ATC/crud/resources/views/profiles/show.blade.php
+++ PMP_ATC/crud/resources/views/profiles/show.blade.php
@@ -322,9 +322,10 @@
                 <div class="form-group">
                   <label for="technology_id">Technology:</label>
                   <select name="technology_id" id="technology_id" class="form-control" required>
-                    <option value="">Select Technology</option>
                     @foreach ($technologies as $technology)
-                      <option value="{{ $technology->id }}">{{ $technology->technology_name }}</option>
+                      <option value="{{ $technology->id }}" {{ $user_technology->technology_id == $technology->id ? 'selected' : '' }}>
+                        {{ $technology->technology_name }}
+                      </option>
                     @endforeach
                   </select>                                             
                 </div>
@@ -333,7 +334,7 @@
               <div class ="col-md-3">
                 <div class="form-group">
                     <label for="years_of_experience">Years Of Experience:</label>
-                    <input type="text" name="years_of_experience" id="years_of_experience" class="form-control" required>
+                    <input type="text" name="years_of_experience" id="years_of_experience" class="form-control" value="{{ old('years_of_experience', $user_technology->years_of_experience) }}" required>
                 </div>                                        
               </div>
 
@@ -341,9 +342,10 @@
                 <div class="form-group">
                   <label for="project_role_id">Role:</label>
                   <select name="project_role_id" id="project_role_id" class="form-control" required>
-                    <option value="">Select Role</option>
                     @foreach ($project_roles as $project_role)
-                      <option value="{{ $project_role->id }}">{{ $project_role->member_role_type }}</option>
+                      <option value="{{ $project_role->id }}" {{ $user_technology->project_role_id == $project_role->id ? 'selected' : '' }}>
+                        {{ $project_role->member_role_type }}
+                      </option>
                     @endforeach
                   </select>                                       
                 </div>
@@ -351,7 +353,7 @@
                                         
               <div class="form-group">
                 <label for="details">Details:</label>
-                <textarea class="form-control" class="shadow-sm" name="details" id="details" required="required"></textarea>
+                <textarea class="form-control" class="shadow-sm" name="details" id="details" value="{{ $user_technology->details}}" required></textarea>
               </div>                                   
                                           
               <div class="col-md-6">
