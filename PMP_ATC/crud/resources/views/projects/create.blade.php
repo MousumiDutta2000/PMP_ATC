0e8d18841358df808493be21027fb2b262d2d036
masc4424 <arrkya22@gmail.com>
2023-07-05 17:12:46 +0530

    #

--- PMP_ATC/crud/resources/views/projects/create.blade.php
+++ PMP_ATC/crud/resources/views/projects/create.blade.php
@@ -217,7 +217,7 @@
                 </div>
             </div>
                                 
-            <!-- <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
+            <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-header p-0">
@@ -261,7 +261,7 @@
                         </div>
                     </div>
                 </div>
-            </div> -->
+            </div>
             </div> 
         </div>
 
