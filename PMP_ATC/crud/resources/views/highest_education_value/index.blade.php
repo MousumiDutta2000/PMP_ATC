2c4cb0024ee569aed161850d966efe8a1359b710
Mousumi Dutta <97080528+MousumiDutta2000@users.noreply.github.com>
2023-07-07 12:47:10 +0530

    Index delete modal add all tables

--- PMP_ATC/crud/resources/views/highest_education_value/index.blade.php
+++ PMP_ATC/crud/resources/views/highest_education_value/index.blade.php
@@ -12,7 +12,9 @@
     <link rel='stylesheet' href='https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css'>
     <link rel='stylesheet' href='https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css'>
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/boxicons@2.0.0/css/boxicons.min.css'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="{{ asset('css/table.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/form.css') }}">
 @endsection
 
 @section('custom_js')
@@ -31,45 +33,61 @@
         <div class="titlebar" style="display: flex; justify-content: flex-end; margin-top: -67px; margin-bottom: 50px; padding: 2px 30px; margin-right: -30px;">
             <a href="{{ route('highest-education-values.create') }}" class="btn btn-primary">Add New</a>
         </div>
-        {{-- @if ($highestEducationValues->count() > 0) --}}
-            <table id="highEducationTable" class="table table-hover responsive" style="width:100%; border-spacing: 0 10px;">
-                <thead>
-                    <tr>
-                        {{-- <th>#</th> --}}
-                        <th style="padding-left:120px;">Highest Education Value</th>
-                        <th>Action</th>
-                    </tr>
-        </thead>
-        <tbody>
-            @foreach ($highestEducationValues as $highestEducationValue)
-            <tr class="shadow" style="border-radius:15px;">
-                {{-- <td>{{ $highestEducationValue->id }}</td> --}}
-                <td style="padding-left:120px;">{{ $highestEducationValue->highest_education_value }}</td>
-                <td>
-                            
-                                <div class="btn-group" role="group">
-                                    <a href="{{ route('highest-education-values.show', ['highest_education_value' => $highestEducationValue->id]) }}">
-                                        <i class="fas fa-eye text-info" style="margin-right: 10px"></i>
-                                    </a>
-                                    <a href="{{ route('highest-education-values.edit', ['highest_education_value' => $highestEducationValue->id]) }}">
-                                        <i class="fas fa-edit text-primary" style="margin-right: 10px"></i>
-                                    </a>
-                                    <form method="post" action="{{ route('highest-education-values.destroy', ['highest_education_value' => $highestEducationValue->id]) }}">
-                                        @method('delete')
-                                        @csrf
-                                        <button type="submit" class="btn btn-link p-0">
-                                            <i class="fas fa-trash-alt text-danger" style="border: none;"></i>
-                                        </button>
-                                    </form>
+        <table id="highEducationTable" class="table table-hover responsive" style="width:100%; border-spacing: 0 10px;">
+            <thead>
+                <tr>
+                    {{-- <th>#</th> --}}
+                    <th style="padding-left:120px;">Highest Education Value</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($highestEducationValues as $highestEducationValue)
+                <tr class="shadow" style="border-radius:15px;">
+                    {{-- <td>{{ $highestEducationValue->id }}</td> --}}
+                    <td style="padding-left:120px;">{{ $highestEducationValue->highest_education_value }}</td>
+                    <td>           
+                        <div class="btn-group" role="group">
+                            <a href="{{ route('highest-education-values.show', ['highest_education_value' => $highestEducationValue->id]) }}">
+                                <i class="fas fa-eye text-info" style="margin-right: 10px"></i>
+                            </a>
+                            <a href="{{ route('highest-education-values.edit', ['highest_education_value' => $highestEducationValue->id]) }}">
+                                <i class="fas fa-edit text-primary" style="margin-right: 10px"></i>
+                            </a>
+                            <form method="post" action="{{ route('highest-education-values.destroy', ['highest_education_value' => $highestEducationValue->id]) }}">
+                                @method('delete')
+                                @csrf
+                                <button type="button" class="btn btn-link p-0 delete-button" data-toggle="modal" data-target="#deleteModal">
+                                    <i class="fas fa-trash-alt text-danger mb-2" style="border: none;"></i>
+                                </button>          
+                                <!-- Delete Modal start -->
+                                <div class="modal fade" id="deleteModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-confirm modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header flex-column">
+                                                <div class="icon-box">
+                                                    <i class="material-icons">&#xE5CD;</i>
+                                                </div>
+                                                <h3 class="modal-title w-100">Are you sure?</h3>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>Do you really want to delete these record?</p>
+                                            </div>
+                                            <div class="modal-footer justify-content-center">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        {{-- @else
-            <p>No verticals found.</p>
-        @endif --}}
+                                <!-- Delete Modal end-->
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </section>
 </main>
 @endsection
--- PMP_ATC/crud/resources/views/vertical/index.blade.php
+++ PMP_ATC/crud/resources/views/vertical/index.blade.php
@@ -13,6 +13,7 @@
     <link rel='stylesheet' href='https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css'>
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/boxicons@2.0.0/css/boxicons.min.css'>
     <link rel="stylesheet" href="{{ asset('css/table.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/form.css') }}">
 @endsection
 
 @section('custom_js')
@@ -22,6 +23,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <script src="{{ asset('js/table.js') }}"></script>
 @endsection
 
@@ -31,50 +33,67 @@
         <div class="titlebar" style="display: flex; justify-content: flex-end; margin-top: -67px; margin-bottom: 50px; padding: 2px 30px; margin-right: -30px;">
             <a href="{{ route('verticals.create') }}" class="btn btn-primary">Add New</a>
         </div>
-        {{-- @if ($verticals->count() > 0) --}}
             <table id="verticalTable" class="table table-hover responsive" style="width:100%; border-spacing: 0 10px;">
                 <thead>
-            <tr>
-                <th>Name</th>
-                <th>Head Name</th>
-                <th>Head Email</th>
-                <th>Head Contact</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($verticals as $vertical)
-            <tr class="shadow" style="border-radius:15px;">
-                <td>{{ $vertical->vertical_name }}</td>
-                <td>{{ $vertical->vertical_head_name }}</td>
-                <td>{{ $vertical->vertical_head_emailId }}</td>
-                <td>{{ $vertical->vertical_head_contact }}</td>
-                <td>
-                            
-                                <div class="btn-group" role="group">
-                                    <a href="{{ route('verticals.show', ['vertical' => $vertical->id]) }}">
-                                        <i class="fas fa-eye text-info" style="margin-right: 10px"></i>
-                                    </a>
-                                    <a href="{{ route('verticals.edit', ['vertical' => $vertical->id]) }}">
-                                        <i class="fas fa-edit text-primary" style="margin-right: 10px"></i>
-                                    </a>
-                                    <form method="post" action="{{ route('verticals.destroy', ['vertical' => $vertical->id]) }}">
-                                        @method('delete')
-                                        @csrf
-                                        <button type="submit" class="btn btn-link p-0">
-                                            <i class="fas fa-trash-alt text-danger" style="border: none;"></i>
-                                        </button>
-                                    </form>
+                    <tr>
+                        <th>Name</th>
+                        <th>Head Name</th>
+                        <th>Head Email</th>
+                        <th>Head Contact</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($verticals as $vertical)
+                <tr class="shadow" style="border-radius:15px;">
+                    <td>{{ $vertical->vertical_name }}</td>
+                    <td>{{ $vertical->vertical_head_name }}</td>
+                    <td>{{ $vertical->vertical_head_emailId }}</td>
+                    <td>{{ $vertical->vertical_head_contact }}</td>
+                    <td>                        
+                        <div class="btn-group" role="group">
+                            <a href="{{ route('verticals.show', ['vertical' => $vertical->id]) }}">
+                                <i class="fas fa-eye text-info" style="margin-right: 10px"></i>
+                            </a>
+
+                            <a href="{{ route('verticals.edit', ['vertical' => $vertical->id]) }}">
+                                <i class="fas fa-edit text-primary" style="margin-right: 10px"></i>
+                            </a>
+
+                            <form method="post" action="{{ route('verticals.destroy', ['vertical' => $vertical->id]) }}">
+                                @method('delete')
+                                @csrf
+                                <button type="button" class="btn btn-link p-0 delete-button" data-toggle="modal" data-target="#deleteModal">
+                                    <i class="fas fa-trash-alt text-danger mb-2" style="border: none;"></i>
+                                </button>          
+                                <!-- Delete Modal start -->
+                                <div class="modal fade" id="deleteModal" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-confirm modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header flex-column">
+                                                <div class="icon-box">
+                                                    <i class="material-icons">&#xE5CD;</i>
+                                                </div>
+                                                <h3 class="modal-title w-100">Are you sure?</h3>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>Do you really want to delete these record?</p>
+                                            </div>
+                                            <div class="modal-footer justify-content-center">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        {{-- @else
-            <p>No verticals found.</p>
-        @endif --}}
+                                <!-- Delete Modal end-->
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </section>
 </main>
 @endsection
-
