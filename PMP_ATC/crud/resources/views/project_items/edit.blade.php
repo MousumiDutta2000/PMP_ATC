4785edea82952389f522d6720bd435c2e381df98
Moupriya <moupriyadas1999@gmail.com>
2023-07-15 00:07:49 +0530

    #

--- PMP_ATC/crud/resources/views/project_items/edit.blade.php
+++ PMP_ATC/crud/resources/views/project_items/edit.blade.php
@@ -23,6 +23,17 @@
 <script src="{{ asset('js/project.js') }}"></script>
 @endsection
 
+@section('custom_js')
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.19/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+    <script src="{{ asset('js/table.js') }}"></script>
+    <script src="{{ asset('js/profiles.js') }}"></script>
+@endsection
+
 @section('content')
 
 <div class="form-container">
