47c4942b7fc9db2dec4057925f7d7c9f3718b20f
Moupriya <moupriyadas1999@gmail.com>
2023-08-07 09:28:30 +0530

    #

--- PMP_ATC/crud/database/migrations/2023_07_20_052947_create_tasks_table.php
+++ PMP_ATC/crud/database/migrations/2023_07_20_052947_create_tasks_table.php
@@ -18,19 +18,26 @@ class CreateTasksTable extends Migration
             $table->text('details');
             // $table->string('attachments')->nullable();
             // $table->unsignedBigInteger('assigned_to');
-            $table->unsignedBigInteger('created_by');
-            $table->unsignedBigInteger('last_edited_by');
-            $table->string('estimated_time'); 
-            $table->string('time_taken');
+
+            // $table->unsignedBigInteger('created_by');
+            // $table->unsignedBigInteger('last_edited_by');
+
+            // $table->string('estimated_time'); 
+            // $table->string('time_taken');
+
             $table->string('status');
-            $table->unsignedBigInteger('parent_task')->nullable();
+            // $table->unsignedBigInteger('parent_task')->nullable();
+
             // $table->foreign('sprint_id')->references('id')->on('sprints');
-            $table->foreign('type')->references('id')->on('project');
-            $table->foreign('parent_task')->references('id')->on('tasks');
+            // $table->foreign('type')->references('id')->on('project');
+            // $table->foreign('parent_task')->references('id')->on('tasks');
+
             $table->string('assigned_to');
+            $table->date('due_date');
             // $table->foreign('assigned_to')->references('id')->on('profiles');
-            $table->foreign('created_by')->references('id')->on('profiles');
-            $table->foreign('last_edited_by')->references('id')->on('profiles');
+
+            // $table->foreign('created_by')->references('id')->on('profiles');
+            // $table->foreign('last_edited_by')->references('id')->on('profiles');
             $table->timestamps();
         });
     }
